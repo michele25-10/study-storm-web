f45e756d7ed6c66047bbb6bad0c357b01b347e78
michele25-10 <michele.gabri01@gmail.com>
2024-03-11 19:02:15 +0100

    pagina confirm-registration

--- src/pages/confirmRegistration/ConfirmRegistration.jsx
+++ src/pages/confirmRegistration/ConfirmRegistration.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 
+import "./confirmRegistration.scss";
+
 const ConfirmRegistration = () => {
     return (
-        <div>
-            asdasdasdsad
-        </div>
+        <div className='confirmRegistration'>
+            <div className="content">
+                <div className="imgHeader">
+                    <img src="/logo.jpg" alt="" />
+                </div>
+                <div className="titleContent">
+                    <h3>Benvenuto Studente!</h3>
+                </div>
+                <div className="contentDescription">
+                    Ciao utente, grazie per aver confermato la tua iscrizione!<br />
+                    Il nostro team è entusiasta di averti in squadra e lavoreremo per migliorare l'applicazione e di conseguenza migliorare il tuo studio.
+                </div>
+            </div>
+        </div >
     )
 }
 
