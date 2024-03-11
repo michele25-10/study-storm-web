a92fb73f3b322d6b2ffb77a2e4dd4f020ad29ded
michele25-10 <michele.gabri01@gmail.com>
2024-03-11 15:53:36 +0100

    Terminato il react-router-dom

--- src/index.js
+++ src/index.js
@@ -3,19 +3,23 @@ import ReactDOM from 'react-dom/client';
 
 import {
   createBrowserRouter,
-  createRoutesFromElements,
-  Route,
   RouterProvider,
 } from "react-router-dom";
+
+//PAGES
 import Presentation from './pages/presentation/Presentation';
+import ConfirmRegistration from './pages/confirmRegistration/ConfirmRegistration';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Presentation />} />
-  )
-);
-
-
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Presentation />,
+  },
+  {
+    path: "/confirm-registration",
+    element: <ConfirmRegistration />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/pages/confirmRegistration/ConfirmRegistration.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ConfirmRegistration = () => {
+    return (
+        <div>
+            asdasdasdsad
+        </div>
+    )
+}
+
+export default ConfirmRegistration
