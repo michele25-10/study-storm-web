206c7a6c82bc226b8332a22d90ba0996b20150da
michele25-10 <michele.gabri01@gmail.com>
2024-03-12 15:28:31 +0100

    collegata API newsletter

--- src/components/footer/function/api.js
+++ src/components/footer/function/api.js
@@ -3,9 +3,17 @@ import { isValidEmail } from "../../../utils/validation";
 
 export const addMailNewsletter = async ({ email }) => {
     if (!isValidEmail(email)) {
-        return false;
+        return true;
     }
 
-    const result = await ws("POST", process.env.REACT_APP_API_ENDPOINT + "/newsletter", null, { email, accepted_cookie: true }, false);
+    const result = await ws("POST",
+        process.env.REACT_APP_API_ENDPOINT + "/newsletter",
+        null,
+        {
+            email,
+            accepted_cookie: true
+        },
+        false
+    );
     return result.error;
 }
