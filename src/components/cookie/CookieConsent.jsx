05d041365e5826fbb0b382dadec6ab96b7a0836f
michele25-10 <michele.gabri01@gmail.com>
2024-07-20 16:14:27 +0200

    Primo settaggio per uso dei cookie

--- src/components/cookie/CookieConsent.jsx
+++ src/components/cookie/CookieConsent.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { useCookies } from 'react-cookie'
 
+import "./cookieConsent.scss";
+
 export default function CookieConsent() {
     const [cookies, setCookies] = useCookies(["cookieConsent"]);
     const giveCookieConsent = () => {
@@ -8,17 +10,24 @@ export default function CookieConsent() {
     };
     const [show, setShow] = useState((true));
 
+    const [openModal, setOpenModal] = useState((false));
+
     return (
-        <div className="cookie-modal" style={show ? null : { display: "none" }}>
-            <p>Utilizziamo servizi di terze parti che memorizzano le informazioni nel dispositivo finale di un visitatore del sito o le recuperano.
-                Successivamente elaboriamo ulteriormente le informazioni. Tutto questo ci aiuta a progettare in modo ottimale e a migliorare continuamente
-                il nostro sito web. Per l'archiviazione, il recupero e l'elaborazione abbiamo bisogno del tuo consenso.
-                Puoi revocare il consenso in qualsiasi momento utilizzando il link nel footer.
-                Ulteriori informazioni sono disponibili nella nostra Informativa sulla privacy.
-                Se hai 16 anni o meno, seleziona "Rifiuta tutto".
-            </p>
-            <button type="button" className="btn btn-outline-success" onClick={giveCookieConsent}>Accetta tutto</button>
-            <button type="button" class="btn btn-outline-danger" onClick={() => setShow(false)} >Rifiuta tutto</button>
-        </div>
+        show ?
+            <div className="cookie-modal fixed-bottom">
+                <p className="content-paragraph">Utilizziamo servizi di terze parti che memorizzano le informazioni nel dispositivo finale di un visitatore del sito o le recuperano.
+                    Successivamente elaboriamo ulteriormente le informazioni. Tutto questo ci aiuta a progettare in modo ottimale e a migliorare continuamente
+                    il nostro sito web. Per l'archiviazione, il recupero e l'elaborazione abbiamo bisogno del tuo consenso.
+                    Puoi revocare il consenso in qualsiasi momento utilizzando il link nel footer.
+                    Ulteriori informazioni sono disponibili nella nostra Informativa sulla privacy.
+                    Se hai 16 anni o meno, seleziona "Rifiuta tutto".<br />
+                    <a href='#' style={{ color: 'white' }} onClick={() => setOpenModal(true)}>Cookie policy</a>
+                </p>
+                <div className="button-control">
+                    <button type="button" className="btn btn-outline-light" onClick={giveCookieConsent}>Accetta tutto</button>
+                    <button type="button" class="btn btn-outline-danger" onClick={() => setShow(false)} >Rifiuta tutto</button>
+                    <button type="button" class="btn btn-outline-secondary" onClick={() => setOpenModal(true)} >Preferenze</button>
+                </div>
+            </div > : <></>
     )
 }
